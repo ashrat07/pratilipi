cc1b17c61c781dc50fa8178fdd5e301e32e85f98
Ashish <ashish.rathore@freecharge.com>
2015-06-26 21:23:38 +0530

    reader version 1

--- src/com/pratilipi/android/ui/ReaderActivity.java
+++ src/com/pratilipi/android/ui/ReaderActivity.java
@@ -1,20 +1,32 @@
 package com.pratilipi.android.ui;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebView;
 
 import com.pratilipi.android.R;
+import com.pratilipi.android.http.HttpGet;
+import com.pratilipi.android.http.HttpResponseListener;
 import com.pratilipi.android.model.Shelf;
+import com.pratilipi.android.util.PConstants;
 import com.pratilipi.android.util.SystemUiHider;
 
-public class ReaderActivity extends FragmentActivity {
+public class ReaderActivity extends Activity implements HttpResponseListener {
+
 	/**
 	 * Whether or not the system UI should be auto-hidden after
 	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -43,6 +55,18 @@ public class ReaderActivity extends FragmentActivity {
 	 */
 	private SystemUiHider mSystemUiHider;
 
+	private WebView mWebView;
+
+	private Shelf mShelf;
+
+	private float mDownX;
+	private float mDownY;
+	private Boolean isOnClick;
+	private float SCROLL_THRESHOLD = 30;
+
+	// private static final int MAX_CLICK_DURATION = 200;
+	// private long startClickTime;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,12 +74,11 @@ public class ReaderActivity extends FragmentActivity {
 		setContentView(R.layout.activity_reader);
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
-		final View contentView = findViewById(R.id.fullscreen_container);
+		mWebView = (WebView) findViewById(R.id.web_view);
 
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
-		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
-				HIDER_FLAGS);
+		mSystemUiHider = SystemUiHider.getInstance(this, mWebView, HIDER_FLAGS);
 		mSystemUiHider.setup();
 		mSystemUiHider
 				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
@@ -97,17 +120,72 @@ public class ReaderActivity extends FragmentActivity {
 					}
 				});
 
-		// Set up the user interaction to manually show or hide the system UI.
-		contentView.setOnClickListener(new View.OnClickListener() {
+		mWebView.setOnTouchListener(new View.OnTouchListener() {
+
 			@Override
-			public void onClick(View view) {
-				if (TOGGLE_ON_CLICK) {
-					mSystemUiHider.toggle();
-				} else {
-					mSystemUiHider.show();
+			public boolean onTouch(View view, MotionEvent motionEvent) {
+				switch (motionEvent.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					mDownX = motionEvent.getX();
+					mDownY = motionEvent.getY();
+					isOnClick = true;
+					break;
+
+				case MotionEvent.ACTION_CANCEL:
+				case MotionEvent.ACTION_UP:
+					if (isOnClick) {
+						if (TOGGLE_ON_CLICK) {
+							mSystemUiHider.toggle();
+						} else {
+							mSystemUiHider.show();
+						}
+					}
+					break;
+
+				case MotionEvent.ACTION_MOVE:
+					if (isOnClick
+							&& (Math.abs(mDownX - motionEvent.getX()) > SCROLL_THRESHOLD || Math
+									.abs(mDownY - motionEvent.getY()) > SCROLL_THRESHOLD)) {
+						isOnClick = false;
+					}
+					break;
+
+				default:
+					break;
 				}
+				return false;
 			}
 		});
+
+		// mWebView.setOnTouchListener(new View.OnTouchListener() {
+		//
+		// @Override
+		// public boolean onTouch(View v, MotionEvent event) {
+		// switch (event.getAction()) {
+		// case MotionEvent.ACTION_DOWN: {
+		// startClickTime = Calendar.getInstance().getTimeInMillis();
+		// break;
+		// }
+		//
+		// case MotionEvent.ACTION_UP: {
+		// long clickDuration = Calendar.getInstance()
+		// .getTimeInMillis() - startClickTime;
+		// if (clickDuration < MAX_CLICK_DURATION) {
+		// if (TOGGLE_ON_CLICK) {
+		// mSystemUiHider.toggle();
+		// } else {
+		// mSystemUiHider.show();
+		// }
+		// }
+		// }
+		//
+		// default:
+		// break;
+		//
+		// }
+		// return false;
+		// }
+		// });
 
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
@@ -119,17 +197,8 @@ public class ReaderActivity extends FragmentActivity {
 			Shelf shelf = (Shelf) getIntent().getExtras()
 					.getParcelable("SHELF");
 			if (shelf != null) {
-				ReaderWebViewFragment fragment = new ReaderWebViewFragment();
-				Bundle bundle = new Bundle();
-				bundle.putParcelable("SHELF", shelf);
-				fragment.setArguments(bundle);
-				FragmentTransaction trans = getSupportFragmentManager()
-						.beginTransaction();
-				trans.replace(R.id.fullscreen_container, fragment,
-						fragment.getCustomTag());
-				trans.addToBackStack(fragment.getCustomTag())
-						.commitAllowingStateLoss();
-				getFragmentManager().executePendingTransactions();
+				mShelf = shelf;
+				requestContent();
 			}
 		}
 	}
@@ -150,6 +219,7 @@ public class ReaderActivity extends FragmentActivity {
 	 * while interacting with activity UI.
 	 */
 	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+
 		@Override
 		public boolean onTouch(View view, MotionEvent motionEvent) {
 			if (AUTO_HIDE) {
@@ -180,6 +250,86 @@ public class ReaderActivity extends FragmentActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.reader_menu, menu);
 		return true;
+	}
+
+	private void requestContent() {
+		HttpGet contentRequest = new HttpGet(this, PConstants.CONTENT_URL);
+
+		HashMap<String, String> requestHashMap = new HashMap<>();
+		requestHashMap.put(PConstants.URL, PConstants.CONTENT_URL);
+		requestHashMap.put("pratilipiId", "" + mShelf.pratilipiId);
+
+		contentRequest.run(requestHashMap);
+	}
+
+	@Override
+	public Boolean setGetStatus(JSONObject finalResult, String getUrl,
+			int responseCode) {
+		if (PConstants.CONTENT_URL.equals(getUrl)) {
+			try {
+				String pageContent = finalResult.getString("pageContent");
+				String font;
+				if (PConstants.CONTENT_LANGUAGE.HINDI.toString().equals(
+						mShelf.language)) {
+					font = "Mangal.ttf";
+				} else if (PConstants.CONTENT_LANGUAGE.TAMIL.toString().equals(
+						mShelf.language)) {
+					font = "Tamil.ttf";
+				} else if (PConstants.CONTENT_LANGUAGE.GUJARATI.toString()
+						.equals(mShelf.language)) {
+					font = "Gujarati.ttf";
+				} else {
+					font = "Montserrat-Regular.ttf";
+				}
+
+				this.copyFile(this, font);
+				mWebView.loadDataWithBaseURL(null,
+						getHtmlData(this, font, pageContent), "text/html",
+						"utf-8", "about:blank");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	private boolean copyFile(Context context, String font) {
+		boolean status = false;
+		try {
+			FileOutputStream out = context.openFileOutput(font,
+					Context.MODE_PRIVATE);
+			InputStream in = context.getAssets().open("fonts/" + font);
+			// Transfer bytes from the input file to the output file
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			// Close the streams
+			out.close();
+			in.close();
+			status = true;
+		} catch (Exception e) {
+			status = false;
+			e.printStackTrace();
+		}
+		return status;
+	}
+
+	private String getHtmlData(Context context, String font, String data) {
+		String head = "<head><style>@font-face { font-family: 'customFont'; src: url('file://"
+				+ context.getFilesDir().getAbsolutePath()
+				+ "/"
+				+ font
+				+ "');}body {font-family: 'customFont';}</style></head>";
+		String htmlData = "<html>" + head + "<body>" + data + "</body></html>";
+		return htmlData;
+	}
+
+	@Override
+	public Boolean setPostStatus(JSONObject finalResult, String postUrl,
+			int responseCode) {
+		return null;
 	}
 
 }
